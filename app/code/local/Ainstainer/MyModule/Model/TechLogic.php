5c427e277b3a1d9319a1ae2e943e4e9546459d7d
Roman Zherebko <rubilnik89@mail.ru>
2016-10-27 17:53:07 +0300

    first commit

---
+++ app/code/local/Ainstainer/MyModule/Model/TechLogic.php
@@ -0,0 +1,8 @@
+<?php
+
+class Ainstainer_MyModule_Model_TechLogic extends Mage_Core_Model_Abstract
+{
+    public function sayHello(){
+        echo 'Hello world!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors', 1);
+require 'app/bootstrap.php';
+require 'app/Mage.php';
+
+Mage::app('admin')->setUseSessionInUrl(false);
+
+umask(0);
+
+echo Mage::getModel('mymodule/techLogic')->sayHello();
